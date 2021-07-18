0916877077443321d005604943a78bc30a0a1705
tanakadatearisa <tanakadatetanakadate@gmail.com>
2021-07-19 03:10:14 +0900

    課題提出テスト
    Task1_2の作成

---
+++ JavaStudy/Task1_2.java
@@ -0,0 +1,24 @@
+
+public class Task1_2 { 
+    public static void main(String[] args) {
+
+        int[] country = new int[3];
+        country[0] = 1;
+        country[1] = 2;
+        country[2] = 3;
+        System.out.println(country[2]);
+        
+        String[] strArray = new String[3];
+        strArray[0] = "りんご";
+        strArray[1] = "もも";
+        strArray[2] = "ぶどう";
+        System.out.println(strArray[1]);
+
+         int[] intArray = { 10, 20, 30, 40, 50 };
+
+        System.out.println(intArray[1] + intArray[4]);
+    }
+    
+}
+
+
