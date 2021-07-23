20459fba4096054461b23fc337ce9358c8f07f79
tanakadatearisa <tanakadatetanakadate@gmail.com>
2021-07-23 21:46:05 +0900

    Initial commit

--- JavaStudy/Task1_2.java
+++ JavaStudy/Task1_2.java
@@ -1,22 +1,33 @@
 
 public class Task1_2 { 
     public static void main(String[] args) {
-
-        int[] country = new int[3];
-        country[0] = 1;
-        country[1] = 2;
-        country[2] = 3;
-        System.out.println(country[2]);
-        
-        String[] strArray = new String[3];
-        strArray[0] = "りんご";
-        strArray[1] = "もも";
-        strArray[2] = "ぶどう";
+        //①「JAPAN」「AMERICA」「KOREA」を要素の値(初期値)とする配列 countryを作成しなさい
+        String[] country = {"JAPAN", "AMERICA","KOREA"};
+        //② ①で作成した配列の要素数を出力してください。
+        System.out.println(country.length);
+        //③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+        //1番目(先頭)の要素に「りんご」を代入
+        //2番目の要素に「もも」を代入
+        //3番目の要素に「ぶどう」を代入
+        String[] strArray = {"りんご", "もも", "ぶどう"};
+        //④ ③で作成した配列の２番目の要素の出力しなさい
         System.out.println(strArray[1]);
 
-         int[] intArray = { 10, 20, 30, 40, 50 };
-
+        //⑤同じ型に複数の値を一つにまとめている
+        int[] intArray = { 10, 20, 30, 40, 50 };
+        //⑥20＋50＝70を出力している
         System.out.println(intArray[1] + intArray[4]);
+        
+
+
+
+        
+        
+        
+
+         
+
+        
     }
     
 }
